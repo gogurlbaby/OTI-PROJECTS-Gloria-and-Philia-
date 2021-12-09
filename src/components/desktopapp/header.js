764aac0c7f08a15cca8cdb1bba7200c662350c3c
Philia Hammond <philiahammmond@gmail.com>
2021-12-09 11:56:39 +0000

    modified header component

--- src/components/desktopapp/header.js
+++ src/components/desktopapp/header.js
@@ -5,7 +5,7 @@ import { Header } from 'semantic-ui-react';
 function Headers() {
     return (
         <div>
-          <Header as='h1' style={{marginTop: '20px', marginRight: '50px', color: 'yellowgreen'}} >
+          <Header as='h1' style={{marginTop: '20px', marginRight: '70px', color: 'yellowgreen'}} >
            OTI VIRTUAL OFFICE
           </Header>
         </div>
